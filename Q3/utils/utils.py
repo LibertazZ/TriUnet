0cae1cb52bc930708c5b2ec6b4714e885350906b
Libertaz Zheng <93639070+LibertazZ@users.noreply.github.com>
2023-09-26 15:03:15 +0800

    Add files via upload

---
+++ Q3/utils/utils.py
@@ -0,0 +1,183 @@
+import numpy as np 
+
+def rain_sample(root, path_zout):
+    Ith, Ath = 35, 256
+    # max_dbz = 0
+    for zout in path_zout:
+        path = root + '/' + zout
+        data = np.load(path)
+        Aev = np.sum(data > Ith)
+        if Aev >= Ath:
+            return True
+    
+    return False
+
+def csi(truth, prediction):
+    map_ = truth > 0
+    truth = truth[map_]
+    prediction = prediction[map_]
+    prediction = prediction > 35
+    truth = truth > 35
+    n11, n10, n01, n00 = 0,0,0,0
+    for i in range(len(truth)):
+        if truth[i] and prediction[i]:
+            n11 += 1
+        elif truth[i] and not prediction[i]:
+            n10 += 1
+        elif not truth[i] and prediction[i]:
+            n01 += 1
+        else:
+            n00 += 1
+    assert n00+n01+n10+n11 == len(truth)
+    return n11, n10, n01, n00
+
+
+
+'''Some helper functions for PyTorch, including:
+    - get_mean_and_std: calculate the mean and std value of dataset.
+    - msr_init: net parameter initialization.
+    - progress_bar: progress bar mimic xlua.progress.
+'''
+import os
+import sys
+import time
+import math
+import logging
+
+import torch.nn as nn
+import torch.nn.init as init
+import torch
+
+def get_mean_and_std(dataset):
+    '''Compute the mean and std value of dataset.'''
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+    print('==> Computing mean and std..')
+    for inputs, targets in dataloader:
+        for i in range(3):
+            mean[i] += inputs[:,i,:,:].mean()
+            std[i] += inputs[:,i,:,:].std()
+    mean.div_(len(dataset))
+    std.div_(len(dataset))
+    return mean, std
+
+def init_params(net):
+    '''Init layer parameters.'''
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            init.kaiming_normal(m.weight, mode='fan_out')
+            if m.bias:
+                init.constant(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            init.constant(m.weight, 1)
+            init.constant(m.bias, 0)
+        elif isinstance(m, nn.Linear):
+            init.normal(m.weight, std=1e-3)
+            if m.bias:
+                init.constant(m.bias, 0)
+
+
+_, term_width = os.popen('stty size', 'r').read().split()
+term_width = int(term_width)
+
+TOTAL_BAR_LENGTH = 65.
+last_time = time.time()
+begin_time = last_time
+def progress_bar(current, total, msg=None):
+    global last_time, begin_time
+    if current == 0:
+        begin_time = time.time()  # Reset for new bar.
+
+    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+
+    sys.stdout.write(' [')
+    for i in range(cur_len):
+        sys.stdout.write('=')
+    sys.stdout.write('>')
+    for i in range(rest_len):
+        sys.stdout.write('.')
+    sys.stdout.write(']')
+
+    cur_time = time.time()
+    step_time = cur_time - last_time
+    last_time = cur_time
+    tot_time = cur_time - begin_time
+
+    L = []
+    L.append('  Step: %s' % format_time(step_time))
+    L.append(' | Tot: %s' % format_time(tot_time))
+    if msg:
+        L.append(' | ' + msg)
+
+    msg = ''.join(L)
+    sys.stdout.write(msg)
+    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
+        sys.stdout.write(' ')
+
+    # Go back to the center of the bar.
+    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
+        sys.stdout.write('\b')
+    sys.stdout.write(' %d/%d ' % (current+1, total))
+
+    if current < total-1:
+        sys.stdout.write('\r')
+    else:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+def format_time(seconds):
+    days = int(seconds / 3600/24)
+    seconds = seconds - days*3600*24
+    hours = int(seconds / 3600)
+    seconds = seconds - hours*3600
+    minutes = int(seconds / 60)
+    seconds = seconds - minutes*60
+    secondsf = int(seconds)
+    seconds = seconds - secondsf
+    millis = int(seconds*1000)
+
+    f = ''
+    i = 1
+    if days > 0:
+        f += str(days) + 'D'
+        i += 1
+    if hours > 0 and i <= 2:
+        f += str(hours) + 'h'
+        i += 1
+    if minutes > 0 and i <= 2:
+        f += str(minutes) + 'm'
+        i += 1
+    if secondsf > 0 and i <= 2:
+        f += str(secondsf) + 's'
+        i += 1
+    if millis > 0 and i <= 2:
+        f += str(millis) + 'ms'
+        i += 1
+    if f == '':
+        f = '0ms'
+    return f
+
+
+def create_logger(save_path='', level='debug'):
+
+    if level == 'debug':
+        _level = logging.DEBUG
+    elif level == 'info':
+        _level = logging.INFO
+
+    logger = logging.getLogger()
+    logger.setLevel(_level)
+
+    cs = logging.StreamHandler()
+    cs.setLevel(_level)
+    logger.addHandler(cs)
+
+    if save_path != '':
+        fh = logging.FileHandler(save_path, mode='w')
+        fh.setLevel(_level)
+
+        logger.addHandler(fh)
+
+    return logger
